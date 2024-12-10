132b3003423cfd99e891b1859ba2f80cd09116b6
Stéphane LOS <slos@hilscher.com>
2024-12-10 17:54:30 +0100

    Work in progress

--- src/models/params.py
+++ src/models/params.py
@@ -1,26 +1,31 @@
 
 import logging
+import pickle
+
 import pandas as pd
 
 from sklearn import model_selection
 from sklearn.ensemble import RandomForestRegressor
 
-
 processed_data_relative_path = "./data/processed_data"
+models_relative_path = "./models"
 
 
-def main(processed_data_rel_path=processed_data_relative_path):
+def main(processed_data_rel_path=processed_data_relative_path,
+         models_rel_path=models_relative_path):
     """
     Find best parameters and save to file.
     """
     logger = logging.getLogger(__name__)
-    logger.info('Normalizing datasets')
+    logger.info('Finding best parameters')
 
     # Import datasets
-    X_train = pd.read_csv(f"{processed_data_rel_path}/X_train.csv", sep=",")
+    X_train = pd.read_csv(f"{processed_data_rel_path}/X_train_scaled.csv",
+                          sep=",")
     X_train = X_train.drop(['date'], axis=1)
 
-    X_test = pd.read_csv(f"{processed_data_rel_path}/X_test.csv", sep=",")
+    X_test = pd.read_csv(f"{processed_data_rel_path}/X_test_scaled.csv",
+                         sep=",")
     X_test = X_test.drop(['date'], axis=1)
 
     y_train = pd.read_csv(f"{processed_data_rel_path}/y_train.csv", sep=",")
@@ -28,31 +33,25 @@ def main(processed_data_rel_path=processed_data_relative_path):
 
     # Define parameter grid
     parameters_rf = {
-        'n_estimators': [100, 200, 300, 1000],
-        'max_depth': [None, 80, 90, 100, 110],
-        'min_samples_split': [2, 5, 10],
-        'min_samples_leaf': [1, 2, 4, 8],
+        'n_estimators': [100, 200, 300],   # , 500, 1000
+        'max_depth': [None],               # , 10, 80, 90, 100, 110
+        'min_samples_split': [2],          # , 5, 10
+        'min_samples_leaf': [1, 2, 4, 8],  # , 16
     }
 
     # Perform grid search
     grid_rf = model_selection.GridSearchCV(
         estimator=RandomForestRegressor(random_state=42),
         param_grid=parameters_rf,
-        scoring='neg_mean_squared_error',
-        cv=3, n_jobs=-1, verbose=2)
+        # scoring='neg_mean_squared_error',
+        cv=5, n_jobs=-1, verbose=2)
 
+    print('grid_rf.fit...')
     grid_rf = grid_rf.fit(X_train.values, y_train.values.ravel())
 
     # Report best score and parameters
     print(f"Best score: {grid_rf.best_score_:.3f}")
     print(grid_rf.best_params_)
-    # {'bootstrap': True, 'max_depth': 80, 'max_features': 3,
-    #  'min_samples_leaf': 3, 'min_samples_split': 8, 'n_estimators': 300}
-
-    # {'bootstrap': True, 'max_depth': 80, 'max_features': 4,
-    #  'min_samples_leaf': 3, 'min_samples_split': 12, 'n_estimators': 100}
-
-    # y_pred = grid_rf.predict(X_test.values)
 
     results = pd.DataFrame(grid_rf.cv_results_)[['params',
                                                  'mean_test_score',
@@ -64,17 +63,9 @@ def main(processed_data_rel_path=processed_data_relative_path):
     test_score = best_model.score(X_test.values, y_test.values.ravel())
     print(f"Test set R^2 score: {test_score:.3f}")
 
-    """
-    rf = RandomForestRegressor(bootstrap=False, max_depth=100,
-                               max_features=2, min_samples_leaf=6,
-                               min_samples_split=12, n_estimators=300)
-
-    rf.fit(X_train.values, y_train.values.ravel())
-    y_pred = rf.predict(X_test.values)
-
-    score_rf = rf.score(X_test.values, y_test.values.ravel())
-    print(score_rf)
-    """
+    # Save model best parameters
+    filename = f"{models_rel_path}/parameters.pkl"
+    pickle.dump(grid_rf.best_params_, open(filename, 'wb'))
 
 
 if __name__ == '__main__':
